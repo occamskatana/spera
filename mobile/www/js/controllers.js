ceb0589ca2e4f07f0dff42fbc9912c4542be17bd
occamskatana <johngallweycarter@gmail.com>
2016-02-06 19:07:28 -0500

    mobile initialized

---
+++ mobile/www/js/controllers.js
@@ -0,0 +1,51 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('LoginCtrl', function($scope, $location, UserSession, $ionicPopup, $rootScope) {
+  $scope.data = {};
+
+  $scope.login = function(){
+    var user_session = new UserSession({user: $scope.data});
+    user_session.$save(
+      function(data){
+        window.localStorage['userId'] = data.id;
+        window.localStorage['userName'] = data.name;
+        $location.path('tab/dash');
+      },
+
+      function(err) {
+        var error = err["data"]["error"] 
+        var confirmPopup = $ionicPopup.alert({
+          title: 'An error occured. Please try again',
+          template: 'error'
+        });
+      }
+      );
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
