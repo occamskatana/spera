e21c61e1df6439b16b737a6b9410ba6392403c85
Chase Merlin <chasemerlin@gmail.com>
2016-02-17 20:52:50 -0600

    boards table

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217025513) do
+ActiveRecord::Schema.define(version: 20160217194524) do
+
+  create_table "boards", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "boards", ["user_id"], name: "index_boards_on_user_id"
 
   create_table "checkins", force: :cascade do |t|
     t.boolean  "use"
@@ -43,6 +53,18 @@ ActiveRecord::Schema.define(version: 20160217025513) do
 
   add_index "objectives", ["goal_id"], name: "index_objectives_on_goal_id"
   add_index "objectives", ["user_id"], name: "index_objectives_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "board_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["board_id"], name: "index_posts_on_board_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
