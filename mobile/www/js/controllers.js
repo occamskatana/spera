9609454452c06fa09646bbdf0dcd9d6feb6f0b38
occamskatana <johngallweycarter@gmail.com>
2016-02-23 20:21:03 -0500

    ui cleanup

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -158,7 +158,6 @@ angular.module('starter.controllers', [])
       Goals.query().$promise.then(function(response) {
         $scope.goals = response.goals
         $scope.userName = localStorage['userName'] 
-        
         });
     }
 
@@ -177,6 +176,34 @@ angular.module('starter.controllers', [])
       $scope.goalData.description = '';
       $state.go('tab.dash');
     };   
+})
+
+.controller('goalPopoverCtrl', function($state, objective, $http, $scope, $ionicPopover){
+  $ionicPopover.fromTemplateUrl('templates/goal.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
 })
 
 .controller('goalCtrl', function($state, $stateParams, objective, $http, $scope){
