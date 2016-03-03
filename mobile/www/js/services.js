20b3fe815bb0ce9b97abf5a26667df0547f047a6
occamskatana <johngallweycarter@gmail.com>
2016-03-02 22:32:11 -0500

    bugfix on servies

--- mobile/www/js/services.js
+++ mobile/www/js/services.js
@@ -29,7 +29,7 @@ angular.module('starter.services', [])
 })
 
 .factory('downVote', function($resource){
-  return $resource("http://localhost:3000/api/v1/boards:board_id/down-vote", {board_id: '@board_id'}, {
+  return $resource("http://localhost:3000/api/v1/boards/:board_id/down-vote", {board_id: '@board_id'}, {
     post: {method: 'POST'},
   })
 })
