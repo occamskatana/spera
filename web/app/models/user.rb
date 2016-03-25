2bbe9848f9996b4ade766f4d235079372b2ebae7
Chase Merlin <chasemerlin@gmail.com>
2016-03-25 11:18:45 -0500

    user data returns path to image, which has a no profile default image

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
     has_many :groupables
     has_many :groups, through: :groupables
     has_many :messages
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ActionController::Base.helpers.asset_path('no-picture-default.jpg')
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
     
