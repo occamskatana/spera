ac51ee8b8786c3b6599b96cc75b73fb266e43806
Chase Merlin <chasemerlin@gmail.com>
2016-03-25 17:28:34 -0500

    don't seed images for now

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -17,8 +17,8 @@
 		first_name: Faker::Name.first_name,
 		last_name: Faker::Name.last_name,
 		email: Faker::Internet.email,
-		password: 'helloworld',
-		avatar: user_images.sample
+		password: 'helloworld'
+	#	avatar: user_images.sample
 		)
 	end
 
