fc3f1ae9a9cb26d32bdc1c0614d90a8844b90dc3
Chase Merlin <chasemerlin@gmail.com>
2016-03-31 21:51:41 -0500

    change schema?

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20160331223939) do
+ActiveRecord::Schema.define(version: 20160331232510) do
 
   create_table "aboutmes", force: :cascade do |t|
     t.integer  "user_id"
