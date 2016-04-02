9901a2c4e91ffa5110a231ffbed099bc4b980bbc
occamskatana <johngallweycarter@gmail.com>
2016-04-01 21:12:43 -0400

    Rawr

--- web/app/models/groupable.rb
+++ web/app/models/groupable.rb
@@ -1,4 +1,12 @@
 class Groupable < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+
+  def joined
+  	self.where(accepted: true)
+  end
+
+  def pending
+  	self.where(accepted: false)
+  end
 end
