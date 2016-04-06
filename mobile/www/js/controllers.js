43e80d9beef9b42707c6ea45421acd7fa1f60df4
occamskatana <johngallweycarter@gmail.com>
2016-04-06 17:45:01 -0400

    sober timer added on home screen

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -393,16 +393,33 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('DashCtrl', function($scope, Goals, $http, $state, Events) {
+.controller('DashCtrl', function($scope, $http, $state) {
 
-   // $scope.loadData = function() { 
-   //    Goals.query().$promise.then(function(response) {
-   //      $scope.goals = response.goals
-   //      $scope.userName = localStorage['userName'] 
-   //      });
-   //  }
+  var soberDate = '1/1/2010';
 
-   //  $scope.loadData();
+  $scope.$on('$ionicView.loaded', function(){
+    upTime(soberDate)
+  })
+
+  function upTime(soberDate) {
+    now = new Date();
+    countTo = new Date(soberDate);
+    difference = (now - countTo);
+
+    days = Math.floor(difference / (60 * 60 * 1000 * 24) * 1);
+    hours = Math.floor((difference % (60 * 60 * 1000 * 24)) / (60 * 60 * 1000) * 1);
+    mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+    secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
+
+    document.getElementById('days').firstChild.nodeValue = days;
+    document.getElementById('hours').firstChild.nodeValue = hours;
+    document.getElementById('minutes').firstChild.nodeValue = mins;
+    document.getElementById('seconds').firstChild.nodeValue = secs;
+
+    clearTimeout(upTime.to);
+    upTime.to = setTimeout(function(){upTime(countTo); }, 1000);
+  }
+
 
 
 })
